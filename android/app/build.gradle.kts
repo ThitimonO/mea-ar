0b082bbc7e66292a284fcdac0d3f87bca540ef66
thitimon.o <thitimon.o@cdg.co.th>
2025-03-17 13:12:05 +0700

    main: update minSdkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.example.mea_flutter_native_views_ar"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = /*flutter.minSdkVersion*/ 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
